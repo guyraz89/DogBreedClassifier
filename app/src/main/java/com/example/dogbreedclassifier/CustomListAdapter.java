3e9b1ff1661a5cb5281037560a733d96575faea6
Nati_Shchiglik <natishch94@gmail.com>
2019-10-11 11:59:41 +0300

    forum

---
+++ app/src/main/java/com/example/dogbreedclassifier/CustomListAdapter.java
@@ -0,0 +1,52 @@
+package com.example.dogbreedclassifier;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomListAdapter extends ArrayAdapter {
+    //to reference the Activity
+    private Activity context;
+
+    //to store the animal images
+    private Integer[] imageIDarray;
+
+    //to store the list of countries
+    private String[] nameArray;
+
+    //to store the list of countries
+    private String[] infoArray;
+
+    public CustomListAdapter(Activity context, String[] nameArrayParam, String[] infoArrayParam, Integer[] imageIDArrayParam){
+
+        super(context,R.layout.forums_listview_row , nameArrayParam);
+
+        this.context=context;
+        this.imageIDarray = imageIDArrayParam;
+        this.nameArray = nameArrayParam;
+        this.infoArray = infoArrayParam;
+
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.forums_listview_row, null,true);
+
+        //this code gets references to objects in the listview_row.xml file
+        TextView nameTextField = (TextView) rowView.findViewById(R.id.nameTextViewID);
+        TextView infoTextField = (TextView) rowView.findViewById(R.id.infoTextViewID);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.rowImage);
+
+        //this code sets the values of the objects to values from the arrays
+        nameTextField.setText(nameArray[position]);
+        infoTextField.setText(infoArray[position]);
+        imageView.setImageResource(imageIDarray[position]);
+
+        return rowView;
+
+    };
+}
